ec4ae6a7863156e263fcfb139d650d104f47c142
pp88194 <hyeomshin@gmail.com>
2022-07-23 13:41:23 +0900

    .

--- FSM/Assets/Scripts/Enemy.cs
+++ FSM/Assets/Scripts/Enemy.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoFSM<Enemy>
 {
+    #region  º¯¼ö
     [SerializeField] float moveSpeed = 1;
     public float MoveSpeed => moveSpeed;
     [SerializeField] float detectionDist = 3;
     public float DetectionDist => detectionDist;
     [HideInInspector] public Player target;
+    #endregion
     private void Awake()
     {
         SetState(new EnemyIdleState());
